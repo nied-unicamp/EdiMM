1018db608e53da9008ed7dc2dc403b8998d6781f
รąмueℓ g.ℓ <samuel_smgl@hotmail.com>
2016-03-29 13:37:22 -0300

    Versão inicial
    Versão inicial do editor Multimodal

---
+++ EditorMm/gera_pdf.php
@@ -0,0 +1,9 @@
+<?php
+require('fpdf.php'); 
+$svg = $_REQUEST["svg"];
+$pdf=new FPDF('L'); 
+$pdf->SetFont('Arial','',14);
+$pdf->AddPage();
+$pdf->Image($svg,0,0,325,200,'PNG');
+$pdf->Output();
+?>
