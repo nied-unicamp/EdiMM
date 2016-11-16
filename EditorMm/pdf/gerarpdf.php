282f98fee2a8e7da5994b9ffa6d7004995b4f45c
xpaulo <paulosergio.pjr@hotmail.com>
2016-11-16 15:00:58 -0200

    Criado primeiro protótipo de site EdMm

--- EditorMm/pdf/gerarpdf.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('pdf.php'); 
-$svg = $_REQUEST["svg"];
-$pdf=new FPDF('L'); 
-$pdf->SetFont('Arial','',14);
-$pdf->AddPage();
-$pdf->Image($svg,0,0,325,200,'PNG');
-$pdf->Output();
-?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# EditorMm
-Editor Multimodal
